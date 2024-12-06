bfda712e1e35a637c75bf848eef4d6cd9e3428b1
EnolaK <enolak64@gmail.com>
2024-12-07 00:08:31 +0100

    added urls

--- urls.js
+++ urls.js
@@ -1,1 +1,3 @@
 export const flashtype = "https://server.flashtype.fr"
+export const discordAvatar = "https://cdn.discordapp.com/avatars/"
+export const discordServer = "https://discord.gg/TSatJ5xDxj"
